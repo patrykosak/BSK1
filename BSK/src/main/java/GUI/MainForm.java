4c752db6abb40d2cdf70239afadae5332df6320b
Patryk Osak <pionchaoo@gmail.com>
2022-03-15 22:23:17 +0100

    button works

--- BSK/src/main/java/GUI/MainForm.java
+++ BSK/src/main/java/GUI/MainForm.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.bsk.GUI;
+package GUI;
 
 /**
  *
@@ -29,42 +29,47 @@ public class MainForm extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        WordTextField = new javax.swing.JTextField();
+        JTypeComboBox = new javax.swing.JComboBox<>();
+        JWordTextField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        RailFenceNTextField = new javax.swing.JTextField();
+        JRailFenceNTextField = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        RailFenceNTextField1 = new javax.swing.JTextField();
+        JRailFenceScoreTextField = new javax.swing.JTextField();
 
         jButton1.setText("jButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Szyfracja", "Deszyfracja" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+        JTypeComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Szyfracja", "Deszyfracja" }));
+        JTypeComboBox.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
+                JTypeComboBoxActionPerformed(evt);
             }
         });
 
-        WordTextField.addActionListener(new java.awt.event.ActionListener() {
+        JWordTextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                WordTextFieldActionPerformed(evt);
+                JWordTextFieldActionPerformed(evt);
             }
         });
 
         jLabel1.setText("Tekst");
 
         jButton2.setText("Sprawdź wyniki");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("Rail fence");
 
-        RailFenceNTextField.addActionListener(new java.awt.event.ActionListener() {
+        JRailFenceNTextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RailFenceNTextFieldActionPerformed(evt);
+                JRailFenceNTextFieldActionPerformed(evt);
             }
         });
 
@@ -72,9 +77,9 @@ public class MainForm extends javax.swing.JFrame {
 
         jLabel4.setText("Wynik");
 
-        RailFenceNTextField1.addActionListener(new java.awt.event.ActionListener() {
+        JRailFenceScoreTextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RailFenceNTextField1ActionPerformed(evt);
+                JRailFenceScoreTextFieldActionPerformed(evt);
             }
         });
 
@@ -90,29 +95,29 @@ public class MainForm extends javax.swing.JFrame {
                             .addComponent(jLabel2)
                             .addComponent(jButton2)
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(WordTextField)
-                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(JWordTextField)
+                                .addComponent(JTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 363, Short.MAX_VALUE))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(156, 156, 156)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(RailFenceNTextField))))
+                            .addComponent(JRailFenceNTextField))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 148, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(RailFenceNTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(JRailFenceScoreTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(54, 54, 54))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(26, 26, 26)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(JTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(10, 10, 10)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(WordTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(JWordTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -120,11 +125,11 @@ public class MainForm extends javax.swing.JFrame {
                             .addComponent(jLabel2)
                             .addComponent(jLabel3))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(RailFenceNTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(JRailFenceNTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(RailFenceNTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(JRailFenceScoreTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 268, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addGap(124, 124, 124))
@@ -147,22 +152,36 @@ public class MainForm extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+    private void JTypeComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTypeComboBoxActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
+    }//GEN-LAST:event_JTypeComboBoxActionPerformed
 
-    private void WordTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_WordTextFieldActionPerformed
+    private void JWordTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JWordTextFieldActionPerformed
         // TODO add your handling code here:
         
-    }//GEN-LAST:event_WordTextFieldActionPerformed
+    }//GEN-LAST:event_JWordTextFieldActionPerformed
 
-    private void RailFenceNTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RailFenceNTextFieldActionPerformed
+    private void JRailFenceNTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JRailFenceNTextFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_RailFenceNTextFieldActionPerformed
+    }//GEN-LAST:event_JRailFenceNTextFieldActionPerformed
 
-    private void RailFenceNTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RailFenceNTextField1ActionPerformed
+    private void JRailFenceScoreTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JRailFenceScoreTextFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_RailFenceNTextField1ActionPerformed
+    }//GEN-LAST:event_JRailFenceScoreTextFieldActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        String word = JWordTextField.getText();
+        int n = Integer.parseInt(JRailFenceNTextField.getText());
+        if(JTypeComboBox.getSelectedItem().equals("Szyfracja")){
+            
+            
+            JRailFenceScoreTextField.setText("");
+        }
+        else{
+            JRailFenceScoreTextField.setText("");
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -200,12 +219,12 @@ public class MainForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField RailFenceNTextField;
-    private javax.swing.JTextField RailFenceNTextField1;
-    private javax.swing.JTextField WordTextField;
+    private javax.swing.JTextField JRailFenceNTextField;
+    private javax.swing.JTextField JRailFenceScoreTextField;
+    private javax.swing.JComboBox<String> JTypeComboBox;
+    private javax.swing.JTextField JWordTextField;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
