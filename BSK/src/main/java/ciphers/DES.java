b15e83487df5d2e3a20a661f96bf29fc72f9b3c3
Patryk Osak <pionchaoo@gmail.com>
2022-03-30 20:28:23 +0200

    generating final keys

--- BSK/src/main/java/ciphers/DES.java
+++ BSK/src/main/java/ciphers/DES.java
@@ -18,7 +18,7 @@ public class DES {
             55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53,
             45, 37, 29, 21, 13, 5, 28, 20, 12, 4 };
     
-     private int[] PC2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+     private static int[] PC2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
             23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55,
             30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29,
             32 };
@@ -95,10 +95,15 @@ public class DES {
              String textAfterInitialPermutation = DES.permute(key, initialPermutation, 64);
              String textLeft = textAfterInitialPermutation.substring(0,32);
              String textRight = textAfterInitialPermutation.substring(32);
-             String keyAfterPC1 = DES.permute(key, PC1, 56);
+             String keyAfterPC1 = permute(key, PC1, 56);
              String C0 = keyAfterPC1.substring(0,28);
              String D0 = keyAfterPC1.substring(28);
-             ArrayList<String> subKeys = DES.generateSubKeys(C0, D0,shift);
+             ArrayList<String> subKeys = generateSubKeys(C0, D0,shift);
+             ArrayList<String> finalKeys = new ArrayList<>();
+             for(int x = 0; x < 16; x++){
+                 String keyPermuted = permute(subKeys.get(x), PC2, 48);
+                 finalKeys.add(keyPermuted);
+             }
              return "";
                  }
          
